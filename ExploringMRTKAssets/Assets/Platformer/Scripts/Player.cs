1ef3418587d14d36766fa26db995a9f7601f5fac
one1lion <i_am_listening@hotmail.com>
2021-12-01 23:30:49 -0700

    Updated git ignore after reverting

--- ExploringMRTKAssets/Assets/Platformer/Scripts/Player.cs
+++ ExploringMRTKAssets/Assets/Platformer/Scripts/Player.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour {
   // SerializeField attribute exposes the variable in the inspector
   // Use it when not exposing to other classes
-  [SerializeField] private Transform _groundCheckTransform;
-  [SerializeField] private LayerMask _playerMask;
+  public Transform _groundCheckTransform;
+  public LayerMask _playerMask;
   private bool _jumpKeyWasPressed;
   private float _horizontalInput;
   private Rigidbody _rigidBodyComponent;
