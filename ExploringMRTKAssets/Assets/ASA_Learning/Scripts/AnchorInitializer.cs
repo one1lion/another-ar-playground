26e948a86f9fe1ef007bd30b6cb930d6043fe971
one1lion <i_am_listening@hotmail.com>
2021-12-08 18:59:32 -0700

    Fixed issues with the edited SpatialAnchorsManager file

--- ExploringMRTKAssets/Assets/ASA_Learning/Scripts/AnchorInitializer.cs
+++ ExploringMRTKAssets/Assets/ASA_Learning/Scripts/AnchorInitializer.cs
@@ -12,28 +12,20 @@ public enum FileConfigType {
   Xml
 }
 
-public class AnchorInitializer : MonoBehaviour {
-  protected static AnchorInitializer instance;
-  public static AnchorInitializer Instance => Instance;
-
+[CreateAssetMenu(fileName = "AnchorInitializer", menuName = "Azure Spatial Anchors/Initializer")]
+public class AnchorInitializer : ScriptableObject {
   [SerializeField]
   protected TextAsset configurationFile;
   public TextAsset ConfigurationFile => configurationFile;
 
-  //[SerializeField]
-  private SpatialAnchorManager spatialAnchorManager;
-  //public SpatialAnchorManager SpatialAnchorManager => spatialAnchorManager;
+  private SpatialAnchorConfig spatialAnchorConfig;
+  public SpatialAnchorConfig SpatialAnchorConfig => spatialAnchorConfig;
 
-  private void Start() {
-    instance = this;
+  private void Awake() {
+    Initialize();
   }
 
   public void Initialize() {
-    if (!TryGetComponent(out spatialAnchorManager)) {
-      throw new MissingComponentException("This GameObject requires the Spatial Anchor Manager component to use the Anchor Initializer");
-    }
-
-    // Set the configuration settings depending on the AuthenticationMode
     if (configurationFile is null) {
       // Ignore the use of this and let it default to the configuration file from the inspector for this GameObject
       Debug.LogWarning("Specifying the Azure Spatial Anchors Configuration is not recommended using the settings in the Inspector.  Instead, provide a TextAsset file containing the serialized or parsable configuration.  The file can be an .json, .ini, or .xml formatted file with this extension.  A sample of each type can be found in Assets/Resources/Asa/");
@@ -43,13 +35,13 @@ public class AnchorInitializer : MonoBehaviour {
     var whiteSpace = new Regex(@"[\r\n\t\s]");
     var noWhiteSpaceText = whiteSpace.Replace(configurationFile.text.ToLower(), "");
     var fileType = noWhiteSpaceText.StartsWith("{") ? FileConfigType.Json // Assume a well-formed JSON file
-      : noWhiteSpaceText.StartsWith("<xml") ? FileConfigType.Xml // Assume an XML file
+      : noWhiteSpaceText.StartsWith("<?xml") ? FileConfigType.Xml // Assume an XML file
         : FileConfigType.Ini; // Assume parsable text where each line is a Key/Value pair, separated by '=' character
 
     Debug.Log($"File type: {fileType}");
     SpatialAnchorConfig sac = default;
 
-    sac = fileType switch {
+    spatialAnchorConfig = fileType switch {
       FileConfigType.Json => ReadJsonSettings(configurationFile.text),
       FileConfigType.Ini => ReadIniSettings(configurationFile.text),
       FileConfigType.Xml => ReadXmlSettings(configurationFile.text),
@@ -64,21 +56,18 @@ public class AnchorInitializer : MonoBehaviour {
       Debug.LogWarning("The provided configuration file could not be read.  Provide a TextAsset file containing the serialized or parsable configuration with an appropriate extension (.json, .ini, or .xml) with properly formatted content.  A sample of each type can be found in Assets/Resources/Asa/");
       return;
     }
-
-    // Apply the settings
-    ApplySpatialAnchorSettings(sac);
   }
 
-  private void ApplySpatialAnchorSettings(SpatialAnchorConfig sam) {
-    spatialAnchorManager.SpatialAnchorsAccountDomain = sam.SpatialAnchorsAccountDomain;
-    switch (sam.AuthenticationMode) {
+  public void ApplySpatialAnchorSettings(SpatialAnchorManager sam) {
+    sam.SpatialAnchorsAccountDomain = spatialAnchorConfig.SpatialAnchorsAccountDomain;
+    switch (spatialAnchorConfig.AuthenticationMode) {
       case AuthenticationMode.ApiKey:
-        spatialAnchorManager.SpatialAnchorsAccountId = sam.SpatialAnchorsAccountId;
-        spatialAnchorManager.SpatialAnchorsAccountKey = sam.SpatialAnchorsAccountKey;
+        sam.SpatialAnchorsAccountId = spatialAnchorConfig.SpatialAnchorsAccountId;
+        sam.SpatialAnchorsAccountKey = spatialAnchorConfig.SpatialAnchorsAccountKey;
         break;
       case AuthenticationMode.AAD:
-        spatialAnchorManager.ClientId = sam.ClientId;
-        spatialAnchorManager.TenantId = sam.TenantId;
+        sam.ClientId = spatialAnchorConfig.ClientId;
+        sam.TenantId = spatialAnchorConfig.TenantId;
         break;
     }
   }
